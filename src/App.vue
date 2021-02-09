2e509c210a0b5b12c1e1d3e501dc323ee2f07af5
Zhalgas Zhumabayev <skrammer777@gmail.com>
2021-02-09 14:46:19 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <div>
+                <delivery v-on:fastLink="changeSide($event)"></delivery>
+                <autocomplete :items="cities" v-on:input="autoSearch($event),listCost()" @input="checkForm" @keyup.enter="onEnter" v-bind:errors="errors"></autocomplete>
+                <shippingcost v-if="delivery.length" :delivery="delivery"></shippingcost>
+                <indexshipping v-else></indexshipping>
+        </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import delivery from "./components/delivery.vue";
+    import indexshipping from "./components/index-shipping.vue";
+    import shippingcost from "./components/shippingcost.vue";
+    import autocomplete from "./components/autocomplete.vue";
+    import axios from 'axios'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            'delivery': delivery,
+            'indexshipping': indexshipping,
+            'shippingcost': shippingcost,
+            'autocomplete': autocomplete,
+        },
+        name: "App",
+        data: function () {
+            return {
+                delivery:{
+                    tyre: [],
+                    required: true,
+                },
+                city: null,
+                cities: [
+                    'Almaty', 'Alatau', 'Almaly', 'Almarasan'
+                ],
+                errors: []
+            }
+        },
+        methods: {
+            onEnter() {
+                if (delivery.length){
+                    this.errors.push('We didn’t found such city. Please check spelling');
+                }
+            },
+            checkForm: function (e) {
+                if (this.city) {
+                    return true;
+                }
+
+                this.errors = [];
+
+                if (!this.city) {
+                    this.errors.push('We didn’t found such city. Please check spelling');
+                }
+
+                e.preventDefault();
+            },
+            listCost: function() {
+                axios.get('https://qvjgl.mocklab.io/delivery/check',{
+                    params: {
+                        search: this.city
+                    }
+                }).then(response => {
+                    this.delivery = response.data
+                }).catch(error => {
+                    this.errorMessage = error.message;
+                    console.error("There was an error!", error);
+                });
+            },
+            changeSide: function (value) {
+                this.delivery = value;
+            },
+            autoSearch: function (value) {
+                this.city = value;
+            }
+
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/shippingcost.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="wrap-shipping-cost">
+        <li class="shipping-cost" v-for="d in delivery" v-bind:class="{ 'inactive': !d.available}" v-on:click="selected = d.type" :key="d.type">
+            <span class="shipping-cost-type">{{ d.type }}</span>
+            <span class="shipping-cost-price">${{ d.price }}</span>
+
+            <img class="shipping-cost-img" :src="`/images/${d.type.toLowerCase()}.svg`" alt="">
+            <div v-if="d.available" class="selected" v-bind:style='{"display": ( d.type == selected ? "block" : "none")}'>
+                <img class="tick" src="/images/tick.svg" alt="tick">
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "shippingcost",
+        props: {
+            delivery: Array,
+        },
+        data: function () {
+          return {
+              selected: undefined,
+          }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
